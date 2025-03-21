48b888c7bc00ea63df5144abbc58305427979d24
Thomas McCall <1032944@student.sae.edu.au>
2025-03-21 12:05:36 +1000

    Blackjack system worked on
    lack of standing

--- Assets/Scripts/BlackjackManager.cs
+++ Assets/Scripts/BlackjackManager.cs
@@ -14,6 +14,12 @@ public class BlackjackManager : MonoBehaviour
         Hearts,
         Diamonds
     }
+    public enum BlackjackState
+    {
+        Idle,
+        Playing
+    }
+    public BlackjackState currentState;
 
     [SerializeField] private GameObject cardPrefab;
     [SerializeField] private Image[] imageList;
@@ -30,19 +36,23 @@ public class BlackjackManager : MonoBehaviour
     [SerializeField] private Button hitButton;
     [SerializeField] private Button standButton;
     [SerializeField] private TMP_InputField betInput;
-    [Header("Cards")]
+    [Space(50)]
     [SerializeField] private List<Card> cards = new();
     [SerializeField] private List<Card> drawnCards = new();
     [SerializeField] private List<Card> dealerDrawnCards = new();
 
+    [SerializeField] private int money = 5000; // Refer this to the actual money value in the script holding it
     private int cardValue = 0;
     private int dealerCardValue = 0;
+    private int cardAceValue = 0;
+    private int dealerCardAceValue = 0;
     private int currentBet = 0;
-    private bool ableToHit = true;
 
     private void CreateDeck()
     {
         cards.Clear();
+        drawnCards.Clear();
+        dealerDrawnCards.Clear();
         for (int deck = 0; deck < deckNumber; deck++)
         {
             for (int suit = 0; suit < 4; suit++)
@@ -60,7 +70,200 @@ public class BlackjackManager : MonoBehaviour
         }
     }
 
+    private void SetState(BlackjackState state)
+    {
+        if (state == currentState) return;
+        currentState = state;
+        switch (currentState)
+        {
+            case BlackjackState.Idle:
+                {
+                    betInput.interactable = true;
+                    drawButton.interactable = true;
+                    doubleButton.interactable = false;
+                    hitButton.interactable = false;
+                    standButton.interactable = false;
+                    break;
+                }
+            case BlackjackState.Playing:
+                {
+                    betInput.interactable = false;
+                    drawButton.interactable = false;
+                    doubleButton.interactable = true;
+                    hitButton.interactable = true;
+                    standButton.interactable = true;
+                    break;
+                }
+        }
+    }
+
+    private void CheckValidityOfDraw()
+    {
+        if (currentState != BlackjackState.Idle)
+        {
+            Debug.Log("Cannot draw while playing");
+            return;
+        }
+        if (betInput.text == "")
+        {
+            Debug.Log("Must bet a value before drawing");
+            return;
+        }
+        int betAmount = Convert.ToInt32(betInput.text);
+        if (betAmount <= 0)
+        {
+            Debug.Log("Cannot bet 0 or a negative amount");
+            return;
+        }
+        if (betAmount > money)
+        {
+            Debug.Log("Cannot bet more than you have");
+            return;
+        }
+        InitialDraw();
+
+    }
+
     private void InitialDraw()
+    {
+        CreateDeck();
+        SetState(BlackjackState.Playing);
+        currentBet = Convert.ToInt32(betInput.text);
+        money -= currentBet;
+        DrawPlayerCard();
+        DrawPlayerCard();
+        DrawDealerCard(false);
+        DrawDealerCard(true);
+        Debug.Log("yippee");
+    }
+
+    private void CheckValidityOfDouble()
+    {
+        if (currentState == BlackjackState.Idle)
+        {
+            Debug.Log("Can only double while playing");
+            return;
+        }
+        if (currentBet > money)
+        {
+            Debug.Log("You do not have enough money to double");
+            return;
+        }
+        Double();
+    }
+
+    private void Double()
+    {
+        money -= currentBet;
+        currentBet *= 2;
+        betInput.text = currentBet.ToString();
+        CheckValidityOfHit();
+        CheckValidityOfStand();
+    }
+
+    private void DrawPlayerCard()
+    {
+        //Check intoxication event
+        Card card = cards[UnityEngine.Random.Range(0, cards.Count)];
+        drawnCards.Add(card);
+        cards.Remove(card);
+        Instantiate(cardPrefab, cardHolder.transform);
+        UpdatePlayerScore();
+    }
+
+    private void DrawDealerCard(bool hidden)
+    {
+        //Check intoxication event
+    }
+
+    private void CheckValidityOfHit()
+    {
+        if (currentState == BlackjackState.Idle)
+        {
+            Debug.Log("Can only hit while playing");
+            return;
+        }
+        Hit();
+    }
+
+    private void Hit()
+    {
+        DrawPlayerCard();
+    }
+
+    private void CheckValidityOfStand()
+    {
+        if (currentState == BlackjackState.Idle)
+        {
+            Debug.Log("Can only stand while playing");
+            return;
+        }
+        Stand();
+    }
+
+    private void Stand()
+    {
+        Debug.Log("stand");
+    }
+
+    private int CountAces(int initialValue, int aces)
+    {
+        int value = initialValue;
+        int currentAces = aces;
+        while (currentAces > 0 && value < 21)
+        {
+            currentAces--;
+            value += 11;
+        }
+        if (value > 21 && currentAces > 0)
+        {
+            value -= 10;
+            while (currentAces > 0)
+            {
+                currentAces--;
+                value++;
+            }
+        }
+        return value;
+    }
+
+    private void UpdatePlayerScore()
+    {
+        int aces = 0;
+        int value = 0;
+        for (int i = 0; i < drawnCards.Count; i++)
+        {
+            if (drawnCards[i] != null)
+            {
+                if (drawnCards[i].number >= 10)
+                {
+                    value += 10;
+                }
+                else if (drawnCards[i].number == 1)
+                {
+                    aces++;
+                }
+                else
+                {
+                    value += drawnCards[i].number;
+                }
+            }
+        }
+        value = CountAces(value, aces);
+        cardValue = value;
+        score.text = value.ToString();
+        if (value > 21)
+        {
+            EndGame(false);
+        }
+    }
+
+    private void UpdateDealerScore()
+    {
+
+    }
+
+    private void EndGame(bool won)
     {
 
     }
@@ -68,14 +271,17 @@ public class BlackjackManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        CreateDeck();
+        drawButton.onClick.AddListener(CheckValidityOfDraw);
+        doubleButton.onClick.AddListener(CheckValidityOfDouble);
+        hitButton.onClick.AddListener(CheckValidityOfHit);
+        standButton.onClick.AddListener(CheckValidityOfStand);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
 
