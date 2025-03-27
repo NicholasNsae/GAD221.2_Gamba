bdbc2a094808eb978551a4327ce09df8096f4cc7
NicholasNsae <1032586@student.sae.edu.au>
2025-03-28 00:25:20 +1000

    || Commit Log ||
    + added events for when drinks are accepts
    + added functions for each outcome of a player response
        - accepting drink and having enough money to buy
        - accepting and not having enough
        - declining drink
        - accepting drink but realise it cost to much
    + added phone manager which holds the players money (no phone mechanics just yet)

--- Assets/Scripts/PhoneManager.cs
+++ Assets/Scripts/PhoneManager.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class PhoneManager : MonoBehaviour
 {
+    [SerializeField] private int bankValue;
+
+    public int BankValue {get; private set;}
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        BankValue = 1000;
     }
 
     // Update is called once per frame
