844ed95a5329b76154c6d360fc639a9893a867c5
Dakota E <62161190+Kaizenoku@users.noreply.github.com>
2025-04-27 13:38:27 +1000

    change game over event to inherit from night event

--- Assets/Scripts/Events/GameOver_Event.cs
+++ Assets/Scripts/Events/GameOver_Event.cs
@@ -1,23 +1,23 @@
 using System;
 using UnityEngine;
 
-public class GameOver_Event : EventBase
+public class GameOver_Event : NightEnded_Event
 {
     public override bool ConditionsMet(EventStats nightStats, EventStats overallStats)
     {   
         if (overallStats.NightsSpentGambling >= 3)
         {
-            EventInfo.EventBodyText = new string[1];
-
             //Mathf.Floor(overallStats.TimeElapsed / 3600f) + ":" +
             //Mathf.Floor((overallStats.TimeElapsed / 60f) % 60f) + ":" +
             //overallStats.TimeElapsed % 60f
 
             string bodyA = $"Time Played: {overallStats.TimeElapsed.ToString("mm:ss")}\n" +
-                $"Total Hands: {overallStats.HandsPlayed}\n" +
-                $"Total Winnings: {overallStats.Winnings}\n" +
-                $"Biggest Winning Streak: {overallStats.BiggestWinningStreak}";
-
+                           $"Total Hands: {overallStats.HandsPlayed}\n" +
+                           $"Biggest Winning Streak: {overallStats.BiggestWinningStreak}" +
+                           $"Total Winnings: ${overallStats.Winnings}\n" +
+                           $"Drinks Had: {overallStats.DrinksHad}";
+            
+            EventInfo.EventBodyText = new string[1];
             EventInfo.EventBodyText[0] = bodyA;
 
             return true;
